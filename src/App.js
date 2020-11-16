11a9ab69c1c5c3d1646cd0788a3b9006ebb9253d
Daisuke <mds87@hotmail.com>
2020-11-16 21:43:29 +0100

    tutorials done till forms

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ class App extends Component{
     constructor(){
         super()
         this.state = {
-
+            firstName: ""
         }
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    // on change function
+    handleChange(event){
+        const firstNameName =
+
+        this.setState({
+            firstName: event.target.value
+        })
     }
 
     render(){
 
         return(
-            <div>
-            </div>
+            <form action="">
+                <input name={this.firstNameName} type="text" placeholder="First Name" onChange={this.handleChange} />
+                <br/>
+                <input name={this.lastNameName} type="text" placeholder="Last Name" onChange={this.handleChange} />
+                <h1>{this.state.firstName}</h1>
+            </form>
         )
     }
 }
