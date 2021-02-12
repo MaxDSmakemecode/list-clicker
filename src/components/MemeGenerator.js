8e755e506634d13ef704d978446a30cbec1bcf2a
Daisuke <mds87@hotmail.com>
2021-02-12 22:40:41 +0100

    finished rewriting meme generator in hooks

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -4,35 +4,13 @@ import React, { useState, useEffect } from 'react'
 // https://api.imgflip.com/get_memes
 
 function MemeGenerator(){
-    const [topText, setTopText] = useState("")
-    const [bottomText, setBottomText] = useState("")
+    const [inputs, setInputs] = useState({
+        topText: "",
+        bottomText: "",
+    })
+    // const handleChange = inputName => ({target}) => setInputs(state => ({...state, [inputName]:target.value}))
     const [randomImg, setRandomImg] = useState("https://picsum.photos/id/237/200/300")
     const [allMemeImgs, setAllMemeImgs] = useState("")
-
-    // constructor(){
-    //     super()
-
-    //     this.state = {
-    //         topText: "",
-    //         bottomText: "",
-    //         randomImg: "https://picsum.photos/id/237/200/300",
-    //         allMemeImgs: "",
-    //     }
-
-    //     this.handleChange = this.handleChange.bind(this)
-    //     this.handleSubmit = this.handleSubmit.bind(this)
-    // }
-
-    // componentDidMount(){
-    //     fetch("https://api.imgflip.com/get_memes")
-    //     .then(response => response.json())
-    //     .then(response => {
-    //         const { memes } = response.data
-    //         this.setState({
-    //             allMemeImgs: memes
-    //         })
-    //     })
-    // }
 
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
@@ -43,49 +21,46 @@ function MemeGenerator(){
         })
     })
 
-    const handleChange = (event) => {
-        setTopText(event.target.value)
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const randomNumber = Math.floor(Math.random() * allMemeImgs.length)
+        const randomMeme = allMemeImgs[randomNumber].url
+        setRandomImg(randomMeme)
     }
-
-    // handleSubmit(event){
-    //     event.preventDefault()
-    //     const randomNumber = Math.floor(Math.random() * this.state.allMemeImgs.length)
-    //     const randomMeme = this.state.allMemeImgs[randomNumber].url
-    //     this.setState({
-    //         randomImg: randomMeme
-    //     })
-    // }
     return(
         <>
             <form 
-            // onSubmit={this.handleSubmit} 
-            className="meme-form">
+                onSubmit={handleSubmit} 
+                className="meme-form"
+            >
                 <input
                     type="text"
+                    key="topText"
                     name="topText" 
-                    value={topText}
+                    value={inputs.topText}
                     placeholder="Top text"
-                    onChange={handleChange}
+                    onChange={({target}) => setInputs(state => ({...state, topText:target.value}))}
                 />
                 <input
                     type="text"
+                    key="bottomText"
                     name="bottomText" 
-                    // value={this.state.bottomText}
+                    value={inputs.bottomText}
                     placeholder="Bottom text"
-                    // onChange={this.handleChange}
+                    onChange={({target}) => setInputs(state => ({...state, bottomText:target.value}))}
                 />
                 <button>Gen</button>
             </form>
             <div className="meme">
                 <img 
-                    // src={randomImg} 
+                    src={randomImg} 
                     alt=""
                 />
                 <h2 className="top">
-                    {topText}
+                    {inputs.topText}
                 </h2>
                 <h2 className="bottom">
-                    {/* {this.state.bottomText} */}
+                    {inputs.bottomText}
                 </h2>
             </div>
         </>
