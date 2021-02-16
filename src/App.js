f7ea3d7982516af4b9db8f38d6479da3e9b0ce25
Daisuke <mds87@hotmail.com>
2021-02-16 22:10:39 +0100

    repeated hooks and made flight booker also in hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,138 @@
-import React, {useState, useEffect} from 'react'
-import Header from './components/Header'
-import MemeGenerator from './components/MemeGenerator'
+import React, {useState} from 'react'
+// import Header from './components/Header'
+// import MemeGenerator from './components/MemeGenerator'
 
 function App(){
+  const [inputs, setInputs] = useState({
+    firstName: "",
+    lastName: "",
+    gender: "",
+    age: "",
+    destination: "",
+    vegetarian: "",
+    kosher: "",
+    lactoseFree: "",
+  })
+
+  const handleChange = inputName => ({target}) => setInputs(state => ({...state, [inputName]:target.value }))
+
+  const handleCheck = inputCheck => ({target}) => setInputs(state => ({...state, [inputCheck]:target.checked}))
+
+  const wrapper = {
+    width: 300,
+    margin: 1 + "rem auto"
+  }
+
+  const inputStyle = {
+    width: 100 + "%",
+    padding: .5 + "rem"
+  }
+
   return(
-    <div>
-      <Header />
-      <MemeGenerator />
+    <div style={wrapper}>
+      <form>
+        <input 
+          style={inputStyle}
+          type="text"
+          key="firstName"
+          name="firstName"
+          value={inputs.firstName}
+          placeholder="First name"
+          onChange={handleChange("firstName")}
+        />
+        <br/><br/>
+        <input 
+          style={inputStyle}
+          type="text"
+          key="lastName"
+          name="lastName"
+          value={inputs.lastName}
+          placeholder="Last name"
+          onChange={handleChange("lastName")}
+        />
+        <br/><br/>
+        <label>
+          <input 
+            type="radio"
+            key="Male"
+            name="gender"
+            value="Male"
+            checked={inputs.gender === "Male"}
+            onChange={handleChange("gender")}
+          /> Male
+        </label>
+        <br/>
+        <label>
+          <input 
+            type="radio"
+            key="Female"
+            name="gender"
+            value="Female"
+            checked={inputs.gender === "Female"}
+            onChange={handleChange("gender")}
+          /> Female
+        </label>
+        <br/><br/>
+        <input 
+          style={inputStyle}
+          type="number"
+          name="age"
+          value={inputs.age}
+          placeholder="Age"
+          onChange={handleChange("age")}
+        />
+        <br/><br/>
+        <select 
+          style={inputStyle}
+          name="destination"
+          value={inputs.destination}
+          onChange={handleChange("destination")}
+        >
+          <option value="Tokyo">Tokyo</option>
+          <option value="Berlin">Berlin</option>
+          <option value="Cancun">Cancun</option>
+        </select>
+        <br/><br/>
+        Dietry restricitons:
+        <br/>
+        <label>
+          <input 
+            type="checkbox"
+            name="vegetarian"
+            checked={inputs.vegetarian}
+            onChange={handleCheck("vegetarian")}
+          /> Vegetarian
+        </label>
+        <br/>
+        <label>
+          <input 
+            type="checkbox"
+            name="kosher"
+            checked={inputs.kosher}
+            onChange={handleCheck("kosher")}
+          /> Kosher
+        </label>
+        <br/>
+        <label>
+          <input 
+            type="checkbox"
+            name="lactoseFree"
+            checked={inputs.lactoseFree}
+            onChange={handleCheck("lactoseFree")}
+          /> Lactose free
+        </label>
+        <br/>
+      </form>
+      <br/><br/>
+      <p>Your name: {inputs.firstName} {inputs.lastName}</p>
+      <p>Your gender: {inputs.gender}</p>
+      <p>Your age: {inputs.age}</p>
+      <p>Your destination: {inputs.destination}</p>
+      <p>Your dietry restrictions: <br/>
+      Vegetarian? {inputs.vegetarian ? "Yes" : "No"}<br/>
+      Kosher? {inputs.kosher ? "Yes" : "No"}<br/>
+      Lactose free? {inputs.lactoseFree ? "Yes" : "No"}
+      </p>
     </div>
   )
 }
--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,70 +1,130 @@
-import React, { useState, useEffect } from 'react'
+// import React, {useState, useEffect} from 'react'
 
 // https://picsum.photos/id/237/200/300
 // https://api.imgflip.com/get_memes
 
-function MemeGenerator(){
-    const [inputs, setInputs] = useState({
-        topText: "",
-        bottomText: "",
-    })
-    // const handleChange = inputName => ({target}) => setInputs(state => ({...state, [inputName]:target.value}))
-    const [randomImg, setRandomImg] = useState("https://picsum.photos/id/237/200/300")
-    const [allMemeImgs, setAllMemeImgs] = useState("")
+// function MemeGenerator(){
+//     const [inputs, setInputs] = useState({
+//         topText: "",
+//         bottomText: "",
+//     })
+//     // const handleChange = inputName => ({target}) => setInputs(state => ({...state, [inputName]:target.value}))
+//     const [randomImg, setRandomImg] = useState("https://picsum.photos/id/237/200/300")
+//     const [allMemeImgs, setAllMemeImgs] = useState("")
 
-    useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
-        .then(response => response.json())
-        .then(response => {
-            const { memes } = response.data
-            setAllMemeImgs(memes)
-        })
-    })
+// useEffect(() => {
+//     fetch("https://api.imgflip.com/get_memes")
+//     .then(response => response.json())
+//     .then(response => {
+//         const { memes } = response.data
+//         setAllMemeImgs(memes)
+//     })
+// })
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        const randomNumber = Math.floor(Math.random() * allMemeImgs.length)
-        const randomMeme = allMemeImgs[randomNumber].url
-        setRandomImg(randomMeme)
-    }
-    return(
-        <>
-            <form 
-                onSubmit={handleSubmit} 
-                className="meme-form"
-            >
-                <input
-                    type="text"
-                    key="topText"
-                    name="topText" 
-                    value={inputs.topText}
-                    placeholder="Top text"
-                    onChange={({target}) => setInputs(state => ({...state, topText:target.value}))}
-                />
-                <input
-                    type="text"
-                    key="bottomText"
-                    name="bottomText" 
-                    value={inputs.bottomText}
-                    placeholder="Bottom text"
-                    onChange={({target}) => setInputs(state => ({...state, bottomText:target.value}))}
-                />
-                <button>Gen</button>
-            </form>
-            <div className="meme">
-                <img 
-                    src={randomImg} 
-                    alt=""
-                />
-                <h2 className="top">
-                    {inputs.topText}
-                </h2>
-                <h2 className="bottom">
-                    {inputs.bottomText}
-                </h2>
-            </div>
-        </>
-    )
+//     const handleSubmit = (event) => {
+//         event.preventDefault()
+//         const randomNumber = Math.floor(Math.random() * allMemeImgs.length)
+//         const randomMeme = allMemeImgs[randomNumber].url
+//         setRandomImg(randomMeme)
+//     }
+//     return(
+//         <>
+//             <form
+//                 onSubmit={handleSubmit}
+//                 className="meme-form"
+//             >
+//                 <input
+//                     type="text"
+//                     key="topText"
+//                     name="topText"
+//                     value={inputs.topText}
+//                     placeholder="Top text"
+//                     onChange={({target}) => setInputs(state => ({...state, topText:target.value}))}
+//                 />
+//                 <input
+//                     type="text"
+//                     key="bottomText"
+//                     name="bottomText"
+//                     value={inputs.bottomText}
+//                     placeholder="Bottom text"
+//                     onChange={({target}) => setInputs(state => ({...state, bottomText:target.value}))}
+//                 />
+//                 <button>Gen</button>
+//             </form>
+//             <div className="meme">
+//                 <img
+//                     src={randomImg}
+//                     alt=""
+//                 />
+//                 <h2 className="top">
+//                     {inputs.topText}
+//                 </h2>
+//                 <h2 className="bottom">
+//                     {inputs.bottomText}
+//                 </h2>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default MemeGenerator
+
+import React, { useState, useEffect } from "react";
+
+function MemeGenerator() {
+  const [inputs, setInputs] = useState({
+    topText: "",
+    bottomText: "",
+  });
+  const handleChange = (inputValue) => ({ target }) => setInputs((state) => ({ ...state, [inputValue]: target.value }));
+
+  const [randomImg, setRandomImg] = useState("https://picsum.photos/id/237/200/300");
+  const [allMemeImgs, setAllMemeImgs] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((response) => {
+        const { memes } = response.data;
+        setAllMemeImgs(memes);
+      });
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const randomNum = Math.floor(Math.random() * allMemeImgs.length)
+    const randomMeme = allMemeImgs[randomNum].url
+    setRandomImg(randomMeme)
+  };
+
+  return (
+    <div>
+      <form className="meme-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          key="topText"
+          name="topText"
+          value={inputs.topText}
+          placeholder="Top text"
+          onChange={handleChange("topText")}
+        />
+        <input
+          type="text"
+          key="bottomText"
+          name="bottomText"
+          value={inputs.bottomText}
+          placeholder="Bottom text"
+          onChange={handleChange("bottomText")}
+        />
+        <button>Gen</button>
+      </form>
+      <div className="meme">
+        <h2 className="top">{inputs.topText}</h2>
+        <h2 className="bottom">{inputs.bottomText}</h2>
+        <img src={randomImg} alt="" />
+      </div>
+    </div>
+  );
 }
 
-export default MemeGenerator
+export default MemeGenerator;
