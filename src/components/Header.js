2ef5eecf5c72129cd366081449d8554a41a8d68c
MaxDSmakemecode <mds87@hotmail.com>
2021-09-08 19:12:00 +0200

    started meme generator after a hiatus

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,3 +19,17 @@
 
 
 // PRACTICE AREA ******************************************************************************
+import React from 'react'
+
+class Header extends React.Component {
+    render(){
+        return(
+            <header>
+                <img src="https://www.hurraki.de/w/images/c/cf/Trollface.jpg" alt="" />
+                <p>Meme Generator</p>
+            </header>
+        )
+    }
+}
+
+export default Header
--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -73,3 +73,68 @@
 
 
 // PRACTICE AREA ******************************************************************************
+import React from 'react'
+
+class MemeGenerator extends React.Component {
+    constructor(){
+        super()
+
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "https://picsum.photos/id/237/200/300",
+            allMemeImgs: "",
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event){
+        const { name, value } = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    componentDidMount(){
+        fetch("https://api.imgflip.com/get_memes")
+        .then(response => response.json())
+        .then(response => this.setState({
+            allMemeImgs: response
+        }))
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <form className="meme-form">
+                    <input 
+                        type="text" 
+                        name="topText"
+                        value={this.state.topText}
+                        onChange={this.handleChange}
+                        placeholder="Top text"
+                    />
+                    <input 
+                        type="text" 
+                        name="bottomText"
+                        value={this.state.bottomText}
+                        onChange={this.handleChange}
+                        placeholder="Bottom text"
+                    />
+                    <button>GEN!</button>
+                </form>
+                <div className="meme">
+                    <img 
+                        src={this.state.randomImg} 
+                        alt="" 
+                    />
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.bottomText}</h2>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default MemeGenerator
